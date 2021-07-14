d7df937f55e62e0554d129c1673453431737e1d7
Sadia <sadiaromain@gmail.com>
2021-07-14 02:14:31 -0400

    simulate infinite scroll

--- src/components/friends-list.tsx
+++ src/components/friends-list.tsx
@@ -13,10 +13,8 @@ export const FriendsList: React.FunctionComponent<IFriendsListProps> = ({
       setHasMore(false);
       return;
     }
-    // a fake async api call like which sends
-    // 20 more records in .5 secs
     setTimeout(() => {
-      setItems(items.concat(Array.from({ length: 6 })));
+      setItems(items => items.concat(images));
     }, 500);
   };
 
@@ -24,39 +22,22 @@ export const FriendsList: React.FunctionComponent<IFriendsListProps> = ({
     <div className="carousel-outer">
       <p className="carousel-friend-count">Friends ({images.length})</p>
       <InfiniteScroll
-        dataLength={images.length} //This is important field to render the next data
+        dataLength={items.length}
         next={fetchData}
         hasMore={hasMore}
         loader={<h4>Loading...</h4>}
         endMessage={
           <p style={{ textAlign: 'center' }}>
-            <b>Yay! You have seen it all</b>
+            <b>Down to the bone! 🦴</b>
           </p>
         }
-        // below props only if you need pull down functionality
-        // refreshFunction={this.refresh}
-        // pullDownToRefresh
-        // pullDownToRefreshThreshold={50}
-        // pullDownToRefreshContent={
-        //   <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
-        // }
-        // releaseToRefreshContent={
-        //   <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
-        // }
       >
-        {images.map((x, i) => (
+        {items.map((x, i) => (
           <div key={'image' + i} className="carousel-image">
             <img src={x} alt="" loading="lazy" />
           </div>
         ))}
       </InfiniteScroll>
-      <section className="carousel">
-        {images.map((x, i) => (
-          <div key={'image' + i} className="carousel-image">
-            <img src={x} alt="" loading="lazy" />
-          </div>
-        ))}
-      </section>
     </div>
   );
 };
